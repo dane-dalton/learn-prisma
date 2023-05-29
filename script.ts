d82db4e3e005b98e6f129926c25b69d45e9ffd98
Dane Dalton <danezdalton1@gmail.com>
2023-05-29 09:00:49 -0500

    Write queries for db, test out prisma CRUD

--- script.ts
+++ script.ts
@@ -2,7 +2,60 @@ import { PrismaClient } from "@prisma/client"
 const prisma = new PrismaClient()
 
 async function main() {
-  await prisma.user.deleteMany()
+  // await prisma.user.createMany({
+  //   data: [
+  //     {
+  //       name: "Dane",
+  //       age: 24,
+  //       email: "dane@test.com",
+  //     },
+  //     {
+  //       name: "Dane",
+  //       age: 23,
+  //       email: "dane@test1.com",
+  //     },
+  //     {
+  //       name: "Dane",
+  //       age: 26,
+  //       email: "dane@test4.com",
+  //     },
+  //   ],
+  // })
+  // const user = await prisma.post.findMany({
+  //   where: {
+  //     author: {
+  //       is: {
+  //         age: 27,
+  //       },
+  //     },
+  //   },
+  // })
+
+  const user = await prisma.user.update({
+    where: {
+      email: "dane@test.com",
+    },
+    data: {
+      userPreference: {
+        disconnect: true,
+      },
+    },
+  })
+
+  // const preference = await prisma.userPreference.create({
+  //   data: {
+  //     emailUpdates: true,
+  //   },
+  // })
+
+  // const user = await prisma.user.findFirst({
+  //   where: {
+  //     name: "NewDane",
+  //   },
+  //   include: { userPreference: true },
+  // })
+
+  console.log(user)
 }
 
 main()
